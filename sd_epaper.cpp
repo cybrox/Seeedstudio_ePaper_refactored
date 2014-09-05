6254397454d4fddace07630cac327ddf2f51a557
Sven Marc Gehring <marc@cybrox.eu>
2014-09-05 09:08:36 +0200

    Finished reformatting sd_epaper

--- sd_epaper.cpp
+++ sd_epaper.cpp
@@ -5,14 +5,7 @@
 #include "ePaperDfs.h"
 
 
-static void spi_on(){
-    SPI.begin();
-}
-
 void sd_epaper::begin(EPD_size sz) {
-    openFileTime = 1;
-    closeFileTime = 1;
-    
     DISP_LEN    = 200;
     DISP_WIDTH  = 96;
     LINE_BYTE = 200/8;
@@ -20,40 +13,9 @@ void sd_epaper::begin(EPD_size sz) {
 }
 
 
-/*********************************************************************************************************
-** Function name:           putPixel
-** Descriptions:            putPixel
-*********************************************************************************************************/
-void sd_epaper::putPixel(int x, int y, unsigned char pixel)
-{
+void sd_epaper::putPixel(int x, int y, unsigned char pixel) {
     int x1 = x;
     int y1 = y;
-    
-    switch(direction)
-    {
-        case DIRLEFT:
-        
-        x = y1;
-        y = SIZE_WIDTH-x1;
-        
-        break;
-        
-        case DIRRIGHT:
-        
-        x = SIZE_LEN - y1;
-        y = x1;
-        break;
-        
-        case DIRDOWN:
-        
-        x = SIZE_LEN - x1;
-        y = SIZE_WIDTH - y1;
-        break;
-        
-        default:
-        
-        ;
-    }
     
     if(x>DISP_LEN || y>DISP_WIDTH)return;
     
@@ -62,24 +24,12 @@ void sd_epaper::putPixel(int x, int y, unsigned char pixel)
     
     int mask = 0x01 << bit;
 
-    if (BLACK == pixel)
-    {
-        sram_image[byte] |= mask;
-    }
-    else
-    {
-        sram_image[byte] &= ~mask;
-    }
-
+    if (BLACK == pixel) sram_image[byte] |= mask;
+    else sram_image[byte] &= ~mask;
 }
 
 
-/*********************************************************************************************************
-** Function name:           begin
-** Descriptions:            begin
-*********************************************************************************************************/
-unsigned char sd_epaper::clear()
-{
+unsigned char sd_epaper::clear() {
     memset(sram_image, 0x00, 5808);
 }
 
--- sd_epaper.h
+++ sd_epaper.h
@@ -4,30 +4,18 @@
 #include "ePaperDfs.h"
 #include "EPD.h"
 
-
 #define BLACK   1
 #define WHITE   0
-
-#define print_sd(X)
-#define println_sd(X)
 
 class sd_epaper {
 
   private:
-    int SIZE_LEN;
-    int SIZE_WIDTH;
     int LINE_BYTE;
-    
     int DISP_LEN;
     int DISP_WIDTH;
-    
-    int openFileTime;
-    int closeFileTime;
-    
-    EPD_DIR direction;
+
 
   public:
-    
     void begin(EPD_size sz);
     void putPixel(int x, int y, unsigned char pixel);
     unsigned char clear();
